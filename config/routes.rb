909e8e772b1cf5af7d4f0965a48f526890ada893
keigo999 <highbridgek5@gmail.com>
2019-03-19 00:50:53 +0900

    LPのtopページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/index'
   get 'home/top'
+  get "home/asmr_top" => "home#asmr_top"
   root "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
