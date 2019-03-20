baa8084677f8bd1660d6c81c5cba6c1b406efbd9
keigo999 <highbridgek5@gmail.com>
2019-03-20 14:28:22 +0900

    検索一覧ヘッダー作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
+  include  HomeHelper
   require 'google/apis/youtube_v3'
+
   
   def top
   end
@@ -8,12 +10,6 @@ class HomeController < ApplicationController
   end
 
   def sound_search
-    youtube = Google::Apis::YoutubeV3::YouTubeService.new
-    youtube.key = "AIzaSyASrJQl-AjvuIQ4nS9w9fbXOie9M0Lc4nM"
-    youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: "asmr", max_results: 1)
-    url = youtube_search_list.to_h
-    @url = url[:items][0][:id][:video_id]
-    @image = url[:items][0][:snippet][:thumbnails][:default][:url]
   end
 
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,3 +1,13 @@
 module HomeHelper
+  
+  def search
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = "AIzaSyASrJQl-AjvuIQ4nS9w9fbXOie9M0Lc4nM"
+    youtube_search_list = youtube.list_searches("id,snippet", type: "video", q: "ヒカキン", max_results: 1)
+    url = youtube_search_list.to_h
+    @url = url[:items][0][:id][:video_id]
+    @image = url[:items][0][:snippet][:thumbnails][:default][:url]
+    @title = url[:items][0][:snippet][:title]
+  end
 end
 
