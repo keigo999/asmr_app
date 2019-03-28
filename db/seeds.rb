3a012fa0e6dddc9f3b5504ab55e44697f6e3c5ca
keigo999 <highbridgek5@gmail.com>
2019-03-28 11:41:19 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+  
+# レビューコメント生産用データ
+  User.create!(name: "ファンA", password: "1234567", id: 1)
+  User.create!(name: "ファンB", password: "1234567", id: 2)
 
-
-  User.create! {name: "ファンA" password: "123456"}
-  User.create! {name: "ファンB" password: "123456"}
-
-
-  10.times do |i|
-    Review.create(asmr: "ppomo", title: "ありがとう#{i}", body: "素晴らしいです!#{i}, user_id: 1)
-    Review.create(asmr: "hatomugi", title: "ありがとう#{i}", body: "素晴らしいです!#{i}, user_id: 1)
-    Review.create(asmr: "cham", title: "ありがとう#{i}", body: "素晴らしいです!#{i}, user_id: 1)
-    Review.create(asmr: "muki", title: "ありがとう#{i}", body: "素晴らしいです!#{i}, user_id: 2)
-    Review.create(asmr: "umino", title: "ありがとう#{i}", body: "素晴らしいです!#{i}, user_id: 2)
-    Review.create(asmr: "jae", title: "ありがとう#{i}", body: "素晴らしいです!#{i}, user_id: 2)
+  10.times do
+    Review.create!(asmr: "ppomo", title: "ありがとう", body: "素晴らしいです!", user_id: 1)
+    Review.create!(asmr: "hatomugi", title: "ありがとう", body: "素晴らしいです!", user_id: 1)
+    Review.create!(asmr: "cham", title: "ありがとう", body: "素晴らしいです!", user_id: 1)
+    Review.create!(asmr: "muki", title: "ありがとう", body: "素晴らしいです!", user_id: 1)
+    Review.create!(asmr: "umino", title: "ありがとう", body: "素晴らしいです!", user_id: 1)
+    Review.create!(asmr: "jae", title: "ありがとう", body: "素晴らしいです!", user_id: 1)
   end
